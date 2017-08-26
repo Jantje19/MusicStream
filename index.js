ac36d6d4242a03ca45989c0d1bdfe558ffb0dec6
Vic <mail@testsite-vic.ml>
2017-08-26 14:30:12 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -69,8 +69,8 @@ const utils = {
 							response.status(200).send(data);
 						}
 					});
-				} else response.sendFile(path);
-			} else response.status(404).send('Error: 404. File not found');
+				} else response.status(200).sendFile(path);
+			} else response.status(404).sendFile(__dirname + '/WebInterface/404.html');
 		});
 	},
 
@@ -120,6 +120,25 @@ const utils = {
 				} else reject('The response is not json, so it is useless');
 			}).catch(err => reject);
 		});
+	},
+
+	humanFileSize: function(bytes, si) {
+		let thresh = si ? 1000 : 1024;
+
+		if(Math.abs(bytes) < thresh)
+			return bytes + ' B';
+
+		let u = -1;
+		const units = si
+		? ['kB','MB','GB','TB','PB','EB','ZB','YB']
+		: ['KiB','MiB','GiB','TiB','PiB','EiB','ZiB','YiB'];
+
+		do {
+			bytes /= thresh;
+			++u;
+		} while(Math.abs(bytes) >= thresh && u < units.length - 1);
+
+		return bytes.toFixed(1) + ' '+ units[u];
 	},
 
 	// I don't want to import modules I can write myself
