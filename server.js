c66f8f0af5331dd567e70c30f6e75538021377fd
jantje19 <mail@testsite-vic.ml>
2017-09-19 18:51:39 +0200

    Test

--- server.js
+++ server.js
@@ -422,6 +422,7 @@ app.post('/updateSettings', (request, response) => {
 			});
 });
 
+console.log("TEST")
 require('./serverVideoHandler.js').start(app, dirname, fileHandler, fs, os, settings, utils, querystring);
 require('./serverAudioHandler.js').start(app, dirname, fileHandler, fs, os, settings, utils, querystring, id3, https, URLModule);
 
