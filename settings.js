afc3645f823d83203fad2b70583583b08e5c5cf3
jantje19 <mail@testsite-vic.ml>
2017-12-31 17:12:45 +0100

    Updates

--- settings.js
+++ settings.js
@@ -1,91 +1,91 @@
 module.exports = {
-	port: {
-		val: 8000,
-		type: 'number',
-		desc: "The port the server runs on. If you don't know what this is, don't change it."
+	"port": {
+		"val": 8000,
+		"type": "number",
+		"desc": "The port the server runs on. If you don't know what this is, don't change it."
 	},
 
-	collectMostListened: {
-		val: true,
-		type: 'bool',
-		desc: 'Store the songs you listened and how many times in a playlist.'
+	"collectMostListened": {
+		"val": true,
+		"type": "bool",
+		"desc": "Store the songs you listened and how many times in a playlist."
 	},
 
-	mostListenedPlaylistName: {
-		val: 'MostListened',
-		type: 'string',
-		desc: 'The name of the playlist that stores the songs you listened and how many times.'
+	"mostListenedPlaylistName": {
+		"val": "MostListened",
+		"type": "string",
+		"desc": "The name of the playlist that stores the songs you listened and how many times."
 	},
 
-	audioFileExtensions: {
-		val: ['.mp3', '.m3a', '.wav'],
-		type: 'mult',
-		desc: 'The accepted audio file extensions.'
+	"audioFileExtensions": {
+		"val": [".mp3", ".m3a", ".wav"],
+		"type": "mult",
+		"desc": "The accepted audio file extensions."
 	},
 
-	videoFileExtensions: {
-		val: ['.mp4'],
-		type: 'mult',
-		desc: 'The accepted video file extensions.'
+	"videoFileExtensions": {
+		"val": [".mp4"],
+		"type": "mult",
+		"desc": "The accepted video file extensions."
 	},
 
-	ignoredAudioFiles: {
-		val: [''],
-		type: 'mult',
-		desc: 'Filter out audio files that appear in the /data/ request. The songs are still accessible through /song/ and via a playlist.'
+	"ignoredAudioFiles": {
+		"val": [""],
+		"type": "mult",
+		"desc": "Filter out audio files that appear in the /data/ request. The songs are still accessible through /song/ and via a playlist."
 	},
 
-	ignoredVideoFiles: {
-		val: [''],
-		type: 'mult',
-		desc: 'Filter out video files that appear in the /data/ request. The videos are still accessible through /video/.'
+	"ignoredVideoFiles": {
+		"val": [""],
+		"type": "mult",
+		"desc": "Filter out video files that appear in the /data/ request. The videos are still accessible through /video/."
 	},
 
-	checkOsHomedirs: {
-		val: true,
-		type: 'bool',
-		desc: "Check the OS home directorys /Music/ and /Videos/ for media files."
+	"checkOsHomedirs": {
+		"val": true,
+		"type": "bool",
+		"desc": "Check the OS home directorys /Music/ and /Videos/ for media files."
 	},
 
-	mediaPaths: {
-		val: [''],
-		type: 'mult',
-		desc: 'The paths the server should check for media files. {homedir} is replaced by the os.homedir.'
+	"mediaPaths": {
+		"val": [""],
+		"type": "mult",
+		"desc": "The paths the server should check for media files. {homedir} is replaced by the os.homedir."
 	},
 
-	updateJsonOnStart: {
-		val: true,
-		type: 'bool',
-		desc: "If true, it will first check the filesystem for songs and video's, then it will start the server."
+	"updateJsonOnStart": {
+		"val": true,
+		"type": "bool",
+		"desc": "If true, it will first check the filesystem for songs and video's, then it will start the server."
 	},
 
-	checkForUpdateOnStart: {
-		val: true,
-		type: 'bool',
-		desc: "If true, it will first check the local version number against Githubs, if it finds a new version it will notify you, otherwise it will just start the server. This makes startup times slower."
+	"checkForUpdateOnStart": {
+		"val": true,
+		"type": "bool",
+		"desc": "If true, it will first check the local version number against Githubs, if it finds a new version it will notify you, otherwise it will just start the server. This makes startup times slower."
 	},
 
-	repeatDefaultOnAudio: {
-		val: false,
-		type: 'bool',
-		desc: 'The audio interface has a repeat button, should that be turned on by default on page load?'
+	"repeatDefaultOnAudio": {
+		"val": false,
+		"type": "bool",
+		"desc": "The audio interface has a repeat button, should that be turned on by default on page load?"
 	},
 
-	shuffleDefaultOnAudio: {
-		val: false,
-		type: 'bool',
-		desc: 'The audio interface has a shuffle button, should that be turned on by default on page load?'
+	"shuffleDefaultOnAudio": {
+		"val": false,
+		"type": "bool",
+		"desc": "The audio interface has a shuffle button, should that be turned on by default on page load?"
 	},
 
-	defaultVolume: {
-		val: 100,
-		type: 'range',
-		desc: 'The volume that defaults on page load.'
+	"defaultVolume": {
+		"val": 100,
+		"type": "range",
+		"desc": "The volume that defaults on page load."
 	},
 
-	autoplayTime: {
-		val: 10,
-		type: 'range',
-		desc: 'The time in seconds you have to wait for the next video to play'
+	"autoplayTime": {
+		"val": 10,
+		"type": "range",
+		"desc": "The amount of time in seconds that has to pass for the next video to start playing."
 	}
 }
